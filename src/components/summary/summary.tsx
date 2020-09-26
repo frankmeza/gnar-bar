720c5a4b8e4d2c5f26fe2c90908346c38cee6d0a
Frank Meza <frankmezapdx@gmail.com>
2020-09-26 14:27:42 -0700

    feat: create summary_items cpx

--- src/components/summary/summary.tsx
+++ src/components/summary/summary.tsx
@@ -1,49 +1,25 @@
 import React, { Fragment } from "react";
 
 // components
-import SummaryItem from "./summary_item";
+import SummaryItems from "./summary_items";
 
 // data-related
 import { SummaryProps } from "./types";
+import { ItemType } from "../../types";
 
 // utils
-import {
-    generateSummaryItemList,
-    getAllTabAmounts,
-} from "../utils/shared_utils";
+import { getAllTabAmounts } from "../utils/shared_utils";
 
 const Summary = (props: SummaryProps) => {
     const { beerList, snackList, wineList } = props;
-
     const totals = getAllTabAmounts(props);
-
-    const beerSummaryItems = Object.values(beerList).map(
-        generateSummaryItemList,
-    );
-
-    const wineSummaryItems = Object.values(wineList).map(
-        generateSummaryItemList,
-    );
-
-    const snackSummaryItems = Object.values(snackList).map(
-        generateSummaryItemList,
-    );
 
     return (
         <Fragment>
             <p>SELECTIONS</p>
-
-            {beerSummaryItems.map(summaryItem => (
-                <SummaryItem {...summaryItem} />
-            ))}
-
-            {wineSummaryItems.map(summaryItem => (
-                <SummaryItem {...summaryItem} />
-            ))}
-
-            {snackSummaryItems.map(summaryItem => (
-                <SummaryItem {...summaryItem} />
-            ))}
+            <SummaryItems itemType={ItemType.BEER} items={beerList} />
+            <SummaryItems itemType={ItemType.WINE} items={wineList} />
+            <SummaryItems itemType={ItemType.SNACK} items={snackList} />
 
             <p>TOTALS</p>
             <pre>{JSON.stringify(totals)}</pre>
---
+++ src/components/summary/summary_items.tsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+// components
+import SummaryItem from "./summary_item";
+
+// data-related
+import { SummaryItemsProps } from "./types";
+
+// utils
+import { generateSummaryItem } from "../utils/shared_utils";
+
+const SummaryItems = (props: SummaryItemsProps) => {
+    const { items } = props;
+
+    const summaryItems = Object.values(items).map(generateSummaryItem);
+
+    return (
+        <Fragment>
+            {summaryItems.map(summaryItem => <SummaryItem {...summaryItem} />)}
+        </Fragment>
+    );
+};
+
+export default SummaryItems;
