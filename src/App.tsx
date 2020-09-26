06cd87c318913f77663d3746cf5ff3afc7950a16
Frank Meza <frankmezapdx@gmail.com>
2020-09-26 01:07:31 -0700

    feat: add, impl app util fns

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 
 // data-related
-import { beerList } from "../beer_data";
+import { beerList, wineList } from "../item_data";
+import { Beer, ItemType, Item, Wine } from "./types";
 
 // components
 import SessionSummary from "./components/session_summary";
+import SelectionList from "./components/drinks/item_selection";
+
+// utils
+import { createItemMap, handleSelectItem } from "./app_utils";
 import "./app.scss";
 
 const App = () => {
-    const [beersSelected, setBeersSelected] = useState()
+    const [availableBeers, setAvailableBeers] = useState([] as Beer[]);
+    const [beersSelected, setBeersSelected] = useState([] as Beer[]);
 
+    const [availableWines, setAvailableWines] = useState([] as Wine[]);
+    const [winesSelected, setWinesSelected] = useState([] as Wine[]);
+
+    // initial data loading
+    useEffect(() => {
+        setAvailableBeers(beerList);
+        setAvailableWines(wineList);
+    }, []);
+
+    const handleSelectBeer = (beer: Item) => {
+        handleSelectItem(beer, beersSelected, setBeersSelected);
+    };
+
+    const handleSelectWine = (wine: Item) => {
+        handleSelectItem(wine, winesSelected, setWinesSelected);
+    };
 
     return (
-        <SessionSummary beers={beerList} />
-        //
-        );
+        <Fragment>
+            <SessionSummary
+                beerList={createItemMap(beersSelected)}
+                wineList={createItemMap(winesSelected)}
+            />
+
+            <SelectionList
+                itemType={ItemType.BEER}
+                itemList={availableBeers}
+                handlerFn={handleSelectBeer}
+            />
+
+            <SelectionList
+                itemType={ItemType.WINE}
+                itemList={availableWines}
+                handlerFn={handleSelectWine}
+            />
+        </Fragment>
+    );
 };
 
 export default App;
---
+++ src/app_utils.tsx
@@ -0,0 +1,41 @@
+import { Item } from "./types";
+
+const BEGINNING = 0;
+const END = -1;
+
+export type ItemMap<T> = {
+    [id: string]: T;
+};
+
+export const handleSelectItem = (
+    item: Item,
+    itemList: Item[],
+    callbackFn: Function,
+) => {
+    const itemIds = itemList.map(item => item.id);
+    const indexOfItem = itemIds.indexOf(item.id);
+    const isItemSelected = indexOfItem > -1;
+
+    if (isItemSelected) {
+        callbackFn([
+            ...itemList.slice(BEGINNING, indexOfItem),
+            ...itemList.slice(indexOfItem, END)
+        ]);
+    } else {
+        callbackFn([item, ...itemList]);
+    }
+};
+
+export const createItemMap = (items: Item[]): ItemMap<Item> => {
+    const itemMap = items.reduce(
+        (acc, item) => {
+            return {
+                ...acc,
+                [item.id]: item,
+            };
+        },
+        {} as ItemMap<Item>,
+    );
+
+    return itemMap;
+};
