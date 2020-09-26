3259f4c7816d14adc336d61fc41246a35ddaa4a9
Frank Meza <frankmezapdx@gmail.com>
2020-09-26 15:27:01 -0700

    feat: add stub test, rm old stub

---
+++ src/components/utils/shared_utils.test.ts
@@ -0,0 +1,9 @@
+import { getAllTabAmounts } from "./shared_utils";
+
+describe("2 + 2", () => {
+    console.log(getAllTabAmounts);
+
+    it("returns 4", () => {
+        expect(2 + 2).toEqual(4);
+    });
+});
