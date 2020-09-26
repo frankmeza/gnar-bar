237ef05323c283b21c2a929557cefd77bb8f6c32
Frank Meza <frankmezapdx@gmail.com>
2020-09-26 14:04:49 -0700

    feat: move types into one file

---
+++ src/components/summary/types.ts
@@ -0,0 +1,20 @@
+import { Item, ItemMap, ItemType } from "../../types";
+
+export type Handler = ((i: Item) => void);
+
+export interface SelectionListProps {
+    readonly handlerFn: Handler;
+    readonly itemType: ItemType;
+    readonly itemList: Item[];
+}
+
+export interface SummaryProps {
+    readonly beerList: ItemMap<Item>;
+    readonly snackList: ItemMap<Item>;
+    readonly wineList: ItemMap<Item>;
+}
+
+export interface SummaryItemProps {
+    readonly cost: number;
+    readonly name: string;
+}
