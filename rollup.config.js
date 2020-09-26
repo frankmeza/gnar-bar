57d1a8a242202a50664e51d34a052e6c355cfadd
Frank Meza <frankmezapdx@gmail.com>
2020-09-25 22:27:22 -0700

    fix: make rollup config okay with react hooks

--- rollup.config.js
+++ rollup.config.js
@@ -8,68 +8,71 @@ import { terser } from "rollup-plugin-terser";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
 
+import react from "react";
+import reactDom from "react-dom";
+
 const isProd = process.env.NODE_ENV === "production";
 
-const extensions = [
-  ".js",
-  ".ts",
-  ".tsx",
-];
+const extensions = [".js", ".ts", ".tsx"];
 
 export default {
-  input: "src/index.tsx",
-  output: {
-    file: "public/index.js",
-    format: "iife",
-  },
-  plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify(
-        isProd ? "production" : "development",
-      ),
-    }),
-    resolve({
-      extensions,
-    }),
-    commonjs({
-      include: /node_modules/,
-    }),
-    babel({
-      extensions,
-      exclude: /node_modules/,
-      babelrc: false,
-      runtimeHelpers: true,
-      presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
-        "@babel/preset-typescript",
-      ],
-      plugins: [
-        "react-require",
-        "@babel/plugin-syntax-dynamic-import",
-        "@babel/plugin-proposal-class-properties",
-        [
-          "@babel/plugin-proposal-object-rest-spread",
-          {
-            useBuiltIns: true,
-          },
-        ],
-        [
-          "@babel/plugin-transform-runtime",
-          {
-            corejs: 3,
-            helpers: true,
-            regenerator: true,
-            useESModules: false,
-          },
-        ],
-      ],
-    }),
-    html({
-      fileName: "index.html",
-      title: "ReactTS Starter",
-      template: ({ title }) => {
-        return `
+    input: "src/index.tsx",
+    output: {
+        file: "public/index.js",
+        format: "iife",
+    },
+    plugins: [
+        replace({
+            "process.env.NODE_ENV": JSON.stringify(
+                isProd ? "production" : "development",
+            ),
+        }),
+        resolve({
+            extensions,
+        }),
+        commonjs({
+            include: "node_modules/**",
+            namedExports: {
+                react: Object.keys(react),
+                "react-dom": Object.keys(reactDom),
+            },
+        }),
+        babel({
+            extensions,
+            exclude: /node_modules/,
+            babelrc: false,
+            runtimeHelpers: true,
+            presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+            ],
+            plugins: [
+                "react-require",
+                "@babel/plugin-syntax-dynamic-import",
+                "@babel/plugin-proposal-class-properties",
+                [
+                    "@babel/plugin-proposal-object-rest-spread",
+                    {
+                        useBuiltIns: true,
+                    },
+                ],
+                [
+                    "@babel/plugin-transform-runtime",
+                    {
+                        corejs: 3,
+                        helpers: true,
+                        regenerator: true,
+                        useESModules: false,
+                    },
+                ],
+            ],
+        }),
+        html({
+            fileName: "index.html",
+            title: "ReactTS Starter",
+            template: ({ title }) => {
+                return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -83,24 +86,22 @@ export default {
 </body>
 </html>
 `;
-      },
-    }),
-    scss({
-      output: "public/index.css",
-    }),
-    isProd && terser(),
-    !isProd &&
-      serve({
-        host: "localhost",
-        port: 3000,
-        open: true,
-        contentBase: [
-          "public",
-        ],
-      }),
-    !isProd &&
-      livereload({
-        watch: "public",
-      }),
-  ],
+            },
+        }),
+        scss({
+            output: "public/index.css",
+        }),
+        isProd && terser(),
+        !isProd &&
+            serve({
+                host: "localhost",
+                port: 3000,
+                open: true,
+                contentBase: ["public"],
+            }),
+        !isProd &&
+            livereload({
+                watch: "public",
+            }),
+    ],
 };
