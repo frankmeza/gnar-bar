b4e0f320ebc81cf776d9e7d9b8fc2f44e9b21053
Frank Meza <frankmezapdx@gmail.com>
2020-09-27 14:04:56 -0700

    feat: get better stub server going

--- server_rust_rocket/rust_rocket/src/main.rs
+++ server_rust_rocket/rust_rocket/src/main.rs
@@ -1,10 +1,53 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene)]
+#![feature(decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use rocket::http::{ContentType, Status};
+use rocket::request::Request;
+use rocket::response;
+use rocket::response::{Responder, Response};
+use rocket_contrib::json::{JsonValue};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct User {
+    pub name: String,
+    pub age: i32,
+}
+
+#[derive(Debug)]
+struct ApiResponse {
+    json: JsonValue,
+    status: Status,
+}
+
+impl<'r> Responder<'r> for ApiResponse {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        return Response::build_from(self.json.respond_to(&req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok();
+    }
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    return "Hello, world!";
+fn index() -> ApiResponse {
+    let user = User {
+        name: String::from("frank "),
+        age: 37,
+    };
+
+    return ApiResponse {
+        json: json!(user),
+        status: Status::Ok,
+    };
 }
 
 fn main() {
