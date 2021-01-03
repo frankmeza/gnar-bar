f0660773fdc5b2a07475141f9ddfd7217df407e2
Frank Meza <frankmezapdx@gmail.com>
2021-01-02 21:05:03 -0800

    change formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_cors::Cors;
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{http::header, web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,16 +9,15 @@ async fn greet(req: HttpRequest) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:10001")
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
-            .wrap(
-                Cors::new()
-                    .allowed_origin("http://localhost:10001")
-                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                    .allowed_header(header::CONTENT_TYPE)
-                    .max_age(3600)
-                    .finish(),
-            )
+            .wrap(cors)
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
