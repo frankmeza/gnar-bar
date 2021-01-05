cab0e64444ed7dda16049ec83bafd023977e2300
GACOSTA1988 <georgeacostar@gmail.com>
2021-01-05 11:55:31 -0800

    add sub header

--- client/src/components/summary/summary.tsx
+++ client/src/components/summary/summary.tsx
@@ -3,6 +3,7 @@ import SummaryItems from "./summary_items";
 import { SummaryProps } from "./types";
 import { ItemType } from "../../common_types";
 import { getAllTabAmounts } from "../utils/shared_utils";
+import "./summary.scss"
 
 const Summary = (props: SummaryProps) => {
     const { beerList, snackList, wineList } = props;
@@ -11,12 +12,12 @@ const Summary = (props: SummaryProps) => {
 
     return (
         <Fragment>
-            <p>SELECTIONS</p>
+            <p className="summary-header">SELECTIONS</p>
             <SummaryItems itemType={ItemType.BEER} items={beerList} />
             <SummaryItems itemType={ItemType.WINE} items={wineList} />
             <SummaryItems itemType={ItemType.SNACK} items={snackList} />
 
-            <p>TOTALS</p>
+            <p className="summary-header">TOTALS</p>
             <pre>{JSON.stringify(totals)}</pre>
         </Fragment>
     );
