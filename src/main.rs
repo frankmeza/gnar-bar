446d9db9ae014db9a6c7e79177adb0bdbb4924bc
Frank Meza <frankmezapdx@gmail.com>
2021-05-02 19:53:51 -0700

    add snack handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_cors::Cors;
 use actix_web::{http::header, App, HttpServer};
+use std::io::Result;
 
 mod dao;
 mod db;
@@ -7,9 +8,10 @@ mod handlers;
 mod models;
 mod queries;
 mod services;
+mod utils;
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     let server = HttpServer::new(move || {
         return App::new()
             .wrap(
@@ -24,7 +26,8 @@ async fn main() -> std::io::Result<()> {
                     .max_age(3600)
                     .finish(),
             )
-            .service(handlers::fetch_producer_list);
+            .service(handlers::fetch_producer_list)
+            .service(handlers::fetch_snack_list);
     })
     .bind(("127.0.0.1", 8080))?
     .run();
