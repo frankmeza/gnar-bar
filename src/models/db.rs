31803d2c81f4a774a74c743aef24707a97436ba6
Frank Meza <frankmezapdx@gmail.com>
2021-05-07 20:41:31 -0700

    add beers models

--- src/models/db.rs
+++ src/models/db.rs
@@ -7,7 +7,6 @@ pub struct ErrorResponse {
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct ProducerDB {
-    pub id: String,
     pub city: String,
     pub country: String,
     pub is_brewery: bool,
@@ -15,19 +14,38 @@ pub struct ProducerDB {
     pub is_winery: bool,
     pub name: String,
     pub state: String,
+    // ids, timestamps
+    pub id: String,
     pub created_at: i64,
     pub updated_at: i64,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct SnackDB {
-    pub id: String,
-    pub producer_id: String,
     pub cost: i32,
     pub name: String,
     pub kind: String,
     pub is_dairy: bool,
     pub is_vegetarian: bool,
+    // ids, timestamps
+    pub id: String,
+    pub producer_id: String,
+    pub created_at: i64,
+    pub updated_at: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct BeerDB {
+    pub abv: i32,
+    pub color: String,
+    pub cost: i32,
+    pub name: String,
+    pub finish: String,
+    pub volume: i32,
+    pub kind: String,
+    // ids, timestamps
+    pub id: String,
+    pub producer_id: String,
     pub created_at: i64,
     pub updated_at: i64,
 }
